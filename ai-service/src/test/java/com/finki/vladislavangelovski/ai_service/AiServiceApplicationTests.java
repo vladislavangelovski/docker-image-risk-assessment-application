3d2d58ecd78b59f013fe28114b74ce4fdd645d0a
Vladislav Angelovski <angelovski2505@gmail.com>
2025-07-27 18:20:20 +0200

    Initialize the services for the application

---
+++ ai-service/src/test/java/com/finki/vladislavangelovski/ai_service/AiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finki.vladislavangelovski.ai_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ scan-service/src/main/java/com/finki/vladislavangelovski/scan_service/ScanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.finki.vladislavangelovski.scan_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanServiceApplication.class, args);
+	}
+
+}
