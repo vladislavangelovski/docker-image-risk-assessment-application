77d4890f0c5f9c68bcb873f051f6633aa1c35759
Vladislav Angelovski <angelovski2505@gmail.com>
2025-10-12 14:08:55 +0200

    Fix database connectivity problem for scan service in ci

--- scan-service/src/test/java/com/finki/vladislavangelovski/scan_service/ScanServiceApplicationTests.java
+++ scan-service/src/test/java/com/finki/vladislavangelovski/scan_service/ScanServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.finki.vladislavangelovski.scan_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ScanServiceApplicationTests {
 
 	@Test
